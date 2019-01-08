e002c287ac7d64fee390005f317b0e9708f74f68
Mateusz Flis <flis-mati@wp.pl>
2019-01-08 13:09:16 +0100

    Sprzątanie

--- PingowanieAndrzeja.py
+++
@@ -1,5 +0,0 @@
-import system
-
-system("ping 192.168.8.96")
-
-czemu sie smiejecie z andrzeja?
--- python.py
+++
@@ -1,9 +0,0 @@
-#edit for python > 3.0
-print ("Hello World")
-
-for i in range(0, 10):
-  print i
-
-def funkcja():
-  print ("Hello World")
-  
